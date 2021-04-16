2934a76802028a6b25d95e9c513f17ddf47916ea
wujinsen <2998714655@qq.com>
2021-04-16 09:19:50 +0800

    commit

--- src/cn/wjs/headdesignmodel/DecoratorModel.java
+++ src/cn/wjs/headdesignmodel/DecoratorModel.java
@@ -1,12 +1,15 @@
 package cn.wjs.headdesignmodel;
 
+/**
+ * 装饰设计模式
+ */
 public class DecoratorModel {
     public static void main(String[] args) {
         Beverage beverage = new Espresso();
         System.out.println(beverage.getDescription() + ", " + beverage.cost());
         Beverage beverage2 = new  DarkRoast();
         beverage2 = new Mocha(beverage2);//Mocha装饰DarkRoast
-        beverage2 = new Soy((beverage2));//Soy装饰DarkRoast
+        beverage2 = new Soy(beverage2);//Soy装饰DarkRoast
         System.out.println(beverage2.getDescription() + ", " + beverage2.cost());
 
     }
---
+++ src/cn/wjs/sql/TimestampTest.java
@@ -0,0 +1,21 @@
+package cn.wjs.sql;
+
+import java.text.SimpleDateFormat;
+
+public class TimestampTest {
+    public static void main(String[] args) throws Exception {
+        byte a = 127;
+        byte b = 127;
+        // a = a + b;
+        System.out.println(a);
+        String str = "20200805";
+        System.out.println(str.substring(0, 4));
+        System.out.println(str.substring(4, 8));
+        // System.out.println(str.substring(8, 12));
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        System.out.println(new java.sql.Timestamp(sdf.parse("2020-08-05").getTime()));
+
+    }
+
+}
